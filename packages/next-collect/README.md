f387ec0d634ed17d449e4d18416bbd143f9a0424
Vladimir Klimontovich <vladimir@jitsu.com>
2022-05-24 10:53:44 -0400

    Added husky integration, fixed turbo build, other small things

---
+++ packages/next-collect/README.md
@@ -0,0 +1,1 @@
+[Read the README of a new repo](https://github.com/jitsucom/next-collect)
--- packages/next-collect/src/drivers/echo.ts
+++ packages/next-collect/src/drivers/echo.ts
@@ -1,6 +1,6 @@
 import { EventSinkDriver } from "../index"
 import { ServerUrl } from "./jitsu"
-import {flatten} from "../tools";
+import { flatten } from "../tools"
 
 const styleTags = {
   underscore: "\x1b[4m",
@@ -37,33 +37,34 @@ type ConsoleStyle = {
   [key in StyleType]: StyleFunc
 }
 
-const style = Object.entries(styleTags)
-    .reduce(
-        (acc, [style, sequence]) => ({ ...acc, [style as StyleType]: (str: string) => `${sequence}${str}${reset}` }),
-        {}
-    ) as ConsoleStyle
-
+const style = Object.entries(styleTags).reduce(
+  (acc, [style, sequence]) => ({ ...acc, [style as StyleType]: (str: string) => `${sequence}${str}${reset}` }),
+  {}
+) as ConsoleStyle
 
 export type EchoDriverOpts = {
   format: "json" | "table"
 }
 
 function toTable(event: Record<keyof any, any>) {
-  const flat = flatten(event, {delimiter: "."});
-  const maxKeyLen = Math.max(...Object.keys(flat).map(k => k.length));
+  const flat = flatten(event, { delimiter: "." })
+  const maxKeyLen = Math.max(...Object.keys(flat).map(k => k.length))
 
-  return Object.entries(flat).map(([key, value], idx) => {
-    const keyStr = key.padEnd(maxKeyLen);
-    return `\t${(idx + 1).toString().padStart(3)}. ${style.bold(keyStr)} : ${value}`;
-  }).join("\n");
+  return Object.entries(flat)
+    .map(([key, value], idx) => {
+      const keyStr = key.padEnd(maxKeyLen)
+      return `\t${(idx + 1).toString().padStart(3)}. ${style.bold(keyStr)} : ${value}`
+    })
+    .join("\n")
 }
 
 export const echoDriver: EventSinkDriver<EchoDriverOpts> = opts => {
-  const format = opts?.format || "table";
+  const format = opts?.format || "table"
   return (event, ctx) => {
     console.log(
-      `${style.bold(style.green('[NextCollect:echo driver]'))} processing event ${style.bold(event.eventType || "uknown_event")}\n` +
-        (format === "json" ? JSON.stringify(event, null, 2) : toTable(event)),
+      `${style.bold(style.green("[NextCollect:echo driver]"))} processing event ${style.bold(
+        event.eventType || "uknown_event"
+      )}\n` + (format === "json" ? JSON.stringify(event, null, 2) : toTable(event))
     )
     return Promise.resolve()
   }
