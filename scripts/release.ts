9aa0b3bcde82d60534557c89d9fdad36457b627f
Vladimir Klimontovich <vladimir@jitsu.com>
2022-05-24 12:12:22 -0400

    Improved release publishing

--- scripts/release.ts
+++ scripts/release.ts
@@ -2,28 +2,20 @@ import * as child_process from "child_process";
 import * as path from "path";
 import * as fs from "fs";
 import * as process from "process";
+import * as JSON5 from "json5";
 
 const minimist = require("minimist");
 
 type Version = {
-  base: string;
-  counter: number;
-  tag?: string;
+  canary: {
+    base: string;
+    npmTag: string;
+  };
 };
 
 function readVersion(): Version {
-  return {
-    tag: "alpha",
-    ...JSON.parse(
-      fs.readFileSync(path.resolve(__dirname, "../.versionrc"), "utf8")
-    ),
-  };
-}
-
-function saveVersion(v: Version) {
-  fs.writeFileSync(
-    path.resolve(__dirname, "../.versionrc"),
-    JSON.stringify(v, null, 2)
+  return JSON5.parse(
+    fs.readFileSync(path.resolve(__dirname, "../.versionrc"), "utf8")
   );
 }
 
@@ -65,44 +57,70 @@ function runProjectCommand(
 function printStdout(stdout: any, prefix: string) {
   return [
     "",
-    ...(stdout
+    ...stdout
       .toString()
       .split("\n")
-      .filter((line: any) => line.toString().trim() !== "")),
+      .filter((line: any) => line.toString().trim() !== ""),
   ].join(`\n${prefix}`);
+}
+
+function getRevision() {
+  return getFromCli("git rev-list --abbrev-commit HEAD").split("\n").length;
 }
 
 async function run(args: any) {
   const version = readVersion();
+
+  if (args.canary) {
+    console.log("Publishing canary release");
+  } else if (args.release) {
+    console.log(`Publishing production release ${args.release}`);
+  } else {
+    throw new Error("Please specify either --canary or --release");
+  }
 
   runProjectCommand(`pnpm whoami`, {
     error: () =>
       `Please make sure you are logged in to npm. Run \`pnpm login\` and then make sure you are logged in with \`pnpm whoami\``,
   });
 
-  const releaseVersion = `${version.base}-${version.tag}-${
-    version.counter + 1
-  }-${getFromCli("git rev-parse --short HEAD")}`;
+  const releaseVersion = args.canary
+    ? `${version.canary.base}.${getRevision()}`
+    : args.release;
+  const gitTag = `v${releaseVersion}`;
+  if (getFromCli(`git tag -l ${gitTag}`).trim() !== "") {
+    throw new Error(
+      `Tag ${gitTag} already exists. Seems like version ${releaseVersion} has already been released.`
+    );
+  }
   runProjectCommand(`pnpm version --ws --no-git-tag-version ${releaseVersion}`);
+  const npmTag = version.canary.npmTag || "canary";
   try {
-    saveVersion({ ...version, counter: version.counter + 1 });
+    if (!args.publish) {
+      console.warn(
+        "Skipping publish, making a dry run. Add --publish to make a real release."
+      );
+    }
     runProjectCommand(
-      `pnpm publish --tag canary --filter next-collect --no-git-checks ${
+      `pnpm publish --tag ${
+        args.canary ? npmTag : "latest"
+      } --filter next-collect --no-git-checks ${
         args.publish ? "" : "--dry-run"
       }`
     );
+    const tagCommand = `git tag -a v${releaseVersion} -m "Release ${releaseVersion}"`;
     if (args.publish) {
-      runProjectCommand(`git add .versionrc`);
-      runProjectCommand(
-          `git commit -m 'chore: saved next version for canary releases, last canary - ${releaseVersion} '`
+      runProjectCommand(tagCommand);
+    } else {
+      console.log(
+        `Because of dry run, not tagging the release. Here is the command that would tag id: ${tagCommand}`
       );
-      runProjectCommand(`git tag -a v${releaseVersion} -m "Release ${releaseVersion}"`);
     }
   } finally {
     try {
       runProjectCommand(`pnpm version --ws --no-git-tag-version 0.0.0`);
     } catch (e) {
-      console.error("Failed to rollback to 0.0.0");
+      console.error("Failed to rollback to 0.0.0", e);
     }
   }
 }
